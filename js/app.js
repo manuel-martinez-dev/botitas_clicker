35c80fe5dc0543475de9e953ec781c04cc1993eb
Manuel-Alejandro-Martinez <manuel.martinez59000@gmail.com>
2018-11-21 18:05:35 +0100

    second commit - refactored code using jquery

--- js/app.js
+++ js/app.js
@@ -2,8 +2,20 @@
  * cat clicker function
  */
 
-//let clicks = 0;
+let clicks = 0;
 
-document.getElementById('photo').addEventListener('click', function(event){
-  console.log('clicked')
-}, false);
+//document.getElementById('photo').addEventListener('click', function(event){
+  //console.log('clicked')
+//}, false);
+
+$(function(){
+  $("img").on("mouseover mouseleave", highlight);
+  $("img").on("click", function(e){
+    clicks++;
+    $("#clicks").html(clicks);
+  })
+});
+
+function highlight() {
+  $("img").toggleClass("highlighted");
+}
